dc43fb353f6820c8b1b78800fd8395fde09ec73c
adrien demaegdt <adriendemaegdt@gmail.com>
2024-01-07 22:29:58 +0100

    seance 4 bis

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
-1) Finir presentation theorique JOIN
-2) Faire deuxieme table reveur avec reveur id en commun 
-3) Exercices JOIN ( LEFT, INNER, OUTTER ) 
+-- 1) Finir presentation theorique JOIN
+-- 2) Faire correction exercice généraux
+-- 2) Faire deuxieme table reveur avec reveur id en commun 
+-- 3) Exercices JOIN ( LEFT, INNER, OUTTER ) 
+
